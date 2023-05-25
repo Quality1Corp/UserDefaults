65645c0d5b377f4152ffb2640d491c064a3d863c
Mikhail Mishurenkov <m.mishurenkov@yandex.ru>
2023-05-25 16:47:38 +0300

    add new contact vc

---
+++ UserDefaults/ViewControllers/NewContactViewController.swift
@@ -0,0 +1,41 @@
+//
+//  NewContactViewController.swift
+//  UserDefaults
+//
+//  Created by MikhaiL on 25.05.2023.
+//
+
+import UIKit
+
+final class NewContactViewController: UIViewController {
+    
+    @IBOutlet var doneButton: UIBarButtonItem!
+    @IBOutlet var firstNameTF: UITextField!
+    @IBOutlet var lastNameTF: UITextField!
+    
+    weak var delegate: NewContactViewControllerDelegate!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let action = UIAction { [weak self] _ in
+            guard let firstName = self?.firstNameTF.text else { return }
+            self?.doneButton.isEnabled = !firstName.isEmpty
+        }
+        firstNameTF.addAction(action, for: .editingChanged)
+    }
+    
+    @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
+        save()
+    }
+    
+    @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
+        dismiss(animated: true)
+    }
+    
+    private func save() {
+        guard let firstName = firstNameTF.text else { return }
+        guard let lastName = lastNameTF.text else { return }
+        delegate.add(contact: "\(firstName) \(lastName)")
+        dismiss(animated: true)
+    }
+}
