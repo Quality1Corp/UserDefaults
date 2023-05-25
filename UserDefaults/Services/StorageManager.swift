892a488bfd67c246064b8a3089a1d61019fada38
Mikhail Mishurenkov <m.mishurenkov@yandex.ru>
2023-05-26 00:18:45 +0300

    work with contact.plist file

--- UserDefaults/Services/StorageManager.swift
+++ UserDefaults/Services/StorageManager.swift
@@ -11,26 +11,25 @@ final class StorageManager {
     static let shared = StorageManager()
     private init() {}
     
-    private let userDefaults = UserDefaults.standard
-    private let key = "contacts"
+    private let fileURL = URL.documentsDirectory.appending(path: "Contact").appendingPathExtension("plist")
     
     func save(contact: Contact) {
         var contacts = fetchContact()
         contacts.append(contact)
-        guard let data = try? JSONEncoder().encode(contacts) else { return }
-        userDefaults.set(data, forKey: key)
+        guard let data = try? PropertyListEncoder().encode(contacts) else { return }
+        try? data.write(to: fileURL, options: .noFileProtection)
     }
     
     func fetchContact() -> [Contact] {
-        guard let data = userDefaults.data(forKey: key) else { return [] }
-        guard let contacts = try? JSONDecoder().decode([Contact].self, from: data) else { return [] }
+        guard let data = try? Data(contentsOf: fileURL) else { return [] }
+        guard let contacts = try? PropertyListDecoder().decode([Contact].self, from: data) else { return [] }
         return contacts
     }
     
     func deleteContact(at index: Int) {
         var contacts = fetchContact()
         contacts.remove(at: index)
-        guard let data = try? JSONEncoder().encode(contacts) else { return }
-        userDefaults.set(data, forKey: key)
+        guard let data = try? PropertyListEncoder().encode(contacts) else { return }
+        try? data.write(to: fileURL, options: .noFileProtection)
     }
 }
